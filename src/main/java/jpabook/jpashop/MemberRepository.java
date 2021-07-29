a049cee48774ff4d08d4f7ebac6eb90c02468e27
KangChanSong <thdrkdcks@naver.com>
2021-07-29 20:49:00 +0900

    환경_설정

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    //스프링이 EntityManager 주입해줌
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
